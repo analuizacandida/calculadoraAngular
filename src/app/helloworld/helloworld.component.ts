34e9b28e22c0d8bf0d367ba25f6733adc3c10529
analuizacandida <analuizacandidavc@gmail.com>
2025-07-09 19:03:58 -0300

    capturando valores

---
+++ src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'hello-world',
+  template: `
+    <h1>Hello World!</h1>
+    <p>Welcome to our Angular application.</p>
+  `,
+
+})
+
+export class HelloWorldComponent{
+
+}
